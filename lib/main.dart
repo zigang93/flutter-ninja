b6af8f32346726836542290b18e15fbe806c80f6
Zi Gang <tanzigang93@gmail.com>
2019-10-09 14:35:33 +0800

    convert to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,39 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Ninja id card'),
+        title: Text('Ninja Profile'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // Image(
-            //   image: AssetImage('assets/zigang.jpg'),
-            // ),
             Center(
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/zigang.jpg'),
@@ -63,7 +77,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox( height: 10.0 ),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 fontFamily: 'Roboto',
                 color: Colors.amberAccent[200],
