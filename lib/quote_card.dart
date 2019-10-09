7cc936161d72715e92ea1983721bd1e0456c247b
Zi Gang <tanzigang93@gmail.com>
2019-10-09 15:40:36 +0800

    add func as parameters in widgets

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -4,7 +4,8 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +30,12 @@ class QuoteCard extends StatelessWidget {
                 fontSize: 16.0,
                 color: Colors.grey[800]
               ),
+            ),
+            SizedBox( height: 8.0),
+            FlatButton.icon(
+              onPressed: delete,
+              label: Text('delete quote'),
+              icon: Icon(Icons.delete)
             )
           ],
         ),
